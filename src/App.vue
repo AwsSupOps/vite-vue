80a43aadb9466510c8f45935e04844133764769f
AwsSupOps <agbalouama@gmail.com>
2024-01-22 16:02:05 +0000

    first commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<script setup>
+  import * as mutations from './graphql/mutations';
+  import { generateClient } from 'aws-amplify/api';
+  import { onMounted, ref } from 'vue';
+
+  const name = ref('');
+  const description = ref('');
+
+  const client = generateClient();
+
+  async function addTodo() {
+    if (!name.value || !description.value) return;
+    const todo = { name: name.value, description: description.value };
+    await client.graphql({
+      query: mutations.createTodo,
+      variables: { input: todo }
+    });
+    name.value = '';
+    description.value = '';
+  }
+</script>
+<template>
+  <div id="app">
+    <h1>Todo App</h1>
+    <input type="text" v-model="name" placeholder="Todo name" />
+    <input type="text" v-model="description" placeholder="Todo description" />
+    <button v-on:click="addTodo">Create Todo</button>
+  </div>
+</template>
---
+++ src/main.js
@@ -0,0 +1,22 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import App from './App.vue'
+
+import { Amplify } from 'aws-amplify';
+import config from './aws-exports.js';
+
+
+//Amplify.configure(config);
+Amplify.configure(config,
+    {
+        API: {
+          GraphQL:  {
+            headers: async () => ({
+              'Authorization': 'custom'
+            })
+          }
+        }
+      });
+
+createApp(App).mount('#app')
