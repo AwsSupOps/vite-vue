475f6b35f9e5084b2d18a27bec080570b898efca
AwsSupOps <agbalouama@gmail.com>
2024-01-29 17:53:13 +0000

    update main.js commit

--- src/main.js
+++ src/main.js
@@ -3,8 +3,13 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 
+//import { Amplify } from 'aws-amplify';
+//import config from './aws-exports.js';
+
 import { Amplify } from 'aws-amplify';
-import config from './aws-exports.js';
+import config from './amplifyconfiguration.json';
+
+Amplify.configure(config);
 
 
 //Amplify.configure(config);
